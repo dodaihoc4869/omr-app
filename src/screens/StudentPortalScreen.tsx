53707b89571377b324ed2849ab2c0f77096e266b
Do Dai Hoc <ttadodaihoc@gmail.com>
2026-09-21 21:02:21 +0700

    Sửa lỗi lọt của 87a6cdf: bỏ 2 dòng prop 'lop' (StudentPortalScreen → KhoiKhacPhuc3CheDo/ModalKhacPhucCauSai) là việc khối lớp dở của phiên khác bị quét vào commit; phần component nhận 'lop' chưa commit ⇒ tsc -b đỏ. Trả lại đúng như trước; chủ việc thêm lại kèm component

--- src/screens/StudentPortalScreen.tsx
+++ src/screens/StudentPortalScreen.tsx
@@ -2153,7 +2153,6 @@ export default function StudentPortalScreen() {
                 dsLichSu={dsLichSu}
                 scriptUrl={scriptUrl}
                 initialCheDo={cheDoKhacPhuc}
-                lop={auth.lop}
               />
             </div>
           </div>
@@ -2391,7 +2390,6 @@ export default function StudentPortalScreen() {
           sbd={auth.sbd}
           tieuDeCa={tieuDeKhacPhucModal}
           cheDoMacDinh={cheDoKhacPhucMacDinh}
-          lop={auth.lop}
           onTaoPhieuXong={(html) => {
             setDsCauSaiKhacPhucModal(null)
             setPhieuHtml(html)
